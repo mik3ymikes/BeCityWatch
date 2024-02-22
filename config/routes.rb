08529c8b0813a13a03109de8ada4ce3c32ed8344
mik3ymikes <mikey.henleys@gmail.com>
2024-02-22 16:14:34 -0600

    committed something out

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,22 @@
 Rails.application.routes.draw do
-  get 'sessions/create'
+  # get 'sessions/create'
 
   scope '/' do
     post 'login', to:'sessions#create'
-  
 end
+
   resources :events 
-  resources :users do
-    end
-    resources :alerts
-  end
+
+
+# check on this right below idk
+resources :alerts 
+resources :users do
+  get 'alerts', to: "users#alerts_index"
+end
+end
+
+# this last known working
+  # resources :users do
+  #   end
+  #   resources :alerts
+  # end
