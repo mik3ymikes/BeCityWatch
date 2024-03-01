8672a42f64e4772177640e47870426281bbf7e1f
mik3ymikes <mikey.henleys@gmail.com>
2024-03-01 11:33:06 -0600

    added title to alerts

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_20_210328) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_01_172930) do
   create_table "alerts", force: :cascade do |t|
     t.integer "user_id", null: false
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
     t.index ["user_id"], name: "index_alerts_on_user_id"
   end
 
