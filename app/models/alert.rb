0b27a374896c2db5f5e6f32e272dde0b2349d230
mik3ymikes <mikey.henleys@gmail.com>
2024-03-01 11:50:59 -0600

    required title it worked

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,5 +1,6 @@
 class Alert < ApplicationRecord
   validates :content, presence: true, length: {maximum: 2000}
+  validates :title, presence: true
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 end
