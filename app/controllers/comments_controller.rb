6083002c5fca2473f27bef1a1d6d848b0613c039
mik3ymikes <mikey.henleys@gmail.com>
2024-03-28 14:45:25 -0500

    trying to set up comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,38 @@
 class CommentsController < ApplicationController
+    before_action :set_alert
+
+    def create
+        comment = @alert.comments.new(comment_params.merge(user: current_user))
+        if comment.save
+          render json: comment, status: :created
+        else
+          render json: comment.errors, status: :unprocessable_entity
+        end
+      end
+
+  def update
+    comment = @alert.comments.find(params[:id])
+    if comment.update(comment_params)
+      render json: comment, status: :ok
+    else
+      render json: comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    comment = @alert.comments.find(params[:id])
+    comment.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_alert
+    @alert = Alert.find(params[:alert_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
 end
