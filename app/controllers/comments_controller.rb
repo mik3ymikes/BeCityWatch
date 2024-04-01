009f9f64fb51c35fe2093adc67c48f81deef8086
mik3ymikes <mikey.henleys@gmail.com>
2024-04-01 13:34:38 -0500

    setting up create update and delete for both alerts and events

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,17 @@
 class CommentsController < ApplicationController
-    before_action :set_alert
-    # before_action :set_commentable
-    before_action :authenticate_request
+  before_action :set_commentable, :authenticate_request
 
-
-
-
-
-    def create
-        comment = @alert.comments.new(comment_params.merge(user: @current_user))
-        if comment.save
-          render json: comment, status: :created
-        else
-          render json: comment.errors, status: :unprocessable_entity
-        end
-      end
-
-        
-
+  def create
+    comment = @commentable.comments.new(comment_params.merge(user: @current_user))
+    if comment.save
+      render json: comment, status: :created
+    else
+      render json: comment.errors, status: :unprocessable_entity
+    end
+  end
 
   def update
-    comment = @alert.comments.find(params[:id])
+    comment = @commentable.comments.find(params[:id])
     if comment.update(comment_params)
       render json: comment, status: :ok
     else
@@ -28,36 +19,102 @@ class CommentsController < ApplicationController
     end
   end
 
-
   def destroy
-    comment = @alert.comments.find(params[:id])
+    comment = @commentable.comments.find(params[:id])
     comment.destroy
     head :no_content
   end
 
-
-
-
-
-
-
-
-
   private
 
-  def set_alert
-    @alert = Alert.find(params[:alert_id])
+  def set_commentable
+    if params[:alert_id]
+      @commentable = Alert.find(params[:alert_id])
+    elsif params[:event_id]
+      @commentable = Event.find(params[:event_id])
+    else
+      render json: { error: 'Specify alert_id or event_id' }, status: :unprocessable_entity
+    end
   end
-
-  def set_event
-    @event = Event.find(params[:event_id])
-  end
-
-
-
 
   def comment_params
     params.require(:comment).permit(:content)
   end
-end
+en
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class CommentsController < ApplicationController
+#     before_action :set_alert
+#     # before_action :set_commentable
+#     before_action :authenticate_request
+
+
+
+
+
+#     def create
+#         comment = @alert.comments.new(comment_params.merge(user: @current_user))
+#         if comment.save
+#           render json: comment, status: :created
+#         else
+#           render json: comment.errors, status: :unprocessable_entity
+#         end
+#       end
+
+        
+
+
+#   def update
+#     comment = @alert.comments.find(params[:id])
+#     if comment.update(comment_params)
+#       render json: comment, status: :ok
+#     else
+#       render json: comment.errors, status: :unprocessable_entity
+#     end
+#   end
+
+
+#   def destroy
+#     comment = @alert.comments.find(params[:id])
+#     comment.destroy
+#     head :no_content
+#   end
+
+
+
+
+
+
+
+
+
+#   private
+
+#   def set_alert
+#     @alert = Alert.find(params[:alert_id])
+#   end
+
+#   def set_event
+#     @event = Event.find(params[:event_id])
+#   end
+
+
+
+
+#   def comment_params
+#     params.require(:comment).permit(:content)
+#   end
+# end
 
