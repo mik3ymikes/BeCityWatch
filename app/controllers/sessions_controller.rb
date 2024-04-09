b44034d587e146e475bc24f14a83ea77c003c430
mik3ymikes <mikey.henleys@gmail.com>
2024-04-08 20:18:57 -0500

    corrected test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
       token = jwt_encode(user_id: user.id)
       render json: { token: token }, status: :ok
     else
-      render json: { error: "Unauthorizeddddd" }, status: :unauthorized
+      render json: { error: "Unauthorized" }, status: :unauthorized
     end
   end
 
