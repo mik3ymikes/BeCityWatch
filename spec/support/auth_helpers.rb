bf68a4007e36ae3f374f7c489d46562900085f73
mik3ymikes <mikey.henleys@gmail.com>
2024-04-09 12:16:33 -0500

    sovled issue!

--- spec/support/auth_helpers.rb
+++ spec/support/auth_helpers.rb
@@ -1,6 +1,6 @@
 module AuthHelpers
     def auth_token_for_user(user)
         JWT.encode({user_id: user.id}, Rails.application.secret_key_base)
-        # JWT.encode({user_id: user.id}, Rails.application.secret_key_base)
+        # JWT.encode({user_id: user.id}, Rails.application.secret_key_base solved issue!! cant use secrets!!)
     end
 end
